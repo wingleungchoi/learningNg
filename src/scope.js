11827a53fe9977bdd7d05ee059b4b729166c7097
wingleungchoi <wingleungchoi@gmail.com>
2015-12-19 23:18:15 +0800

    create a loop to check

---
+++ src/scope.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _ = require('lodash');
+
+function Scope () {
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn,
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  var newValue, oldValue;
+  _.forEach(this.$$watchers, function(watcher){
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue, 
+        (oldValue === newValue ? newValue : oldValue),
+      self);
+    };
+  });
+};
+
+module.exports = Scope;
